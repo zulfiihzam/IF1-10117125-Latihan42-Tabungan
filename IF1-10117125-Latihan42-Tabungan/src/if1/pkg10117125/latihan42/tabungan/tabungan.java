5dba244bd51ed24ba7dee47a4cf0646fc6ed0a11
zulfiihzam <35595962+zulfiihzam@users.noreply.github.com>
2020-11-15 11:44:15 +0700

    Add files via upload

---
+++ IF1-10117125-Latihan42-Tabungan/src/if1/pkg10117125/latihan42/tabungan/tabungan.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10117125.latihan42.tabungan;
+
+/**
+ *
+ * @author Zulfi
+ */
+public class tabungan {
+    private int saldo;
+    
+    
+    public int tabungan(int saldo) {
+         return saldo; 
+    }
+
+    public int ambilUang(int jumlah) {
+        return jumlah;
+    }
+}
